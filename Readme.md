3b62335d14ebac568a7480b6d42501aad0dfd2c1
Joana Tria <joana.tria@GXCHLAP00383.local>
2025-06-30 15:39:50 +0800

    Final Submission, Lab 2: Membership Calculator

---
+++ Readme.md
@@ -0,0 +1,8 @@
+## Joana S. Tria
+# Lab 2: Membership Calculator
+
+Sample Output 1:
+![img.png](img.png)
+
+Sample Output 2:
+![img_2.png](img_2.png)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,59 @@
 package org.example;
 import java.util.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static float calculatePrice (String status, int age) {
+        float price = 0;
 
+        if (status.equalsIgnoreCase("Regular")) {
+            if (age < 18) {
+                price = 50.00F;
+            } else if (age >= 18 && age <= 64) {
+                price = 100.00F;
+            } else {
+                price = 75.00F;
+            }
+        }else if (status.equalsIgnoreCase("VIP")) {
+            if (age < 18) {
+                price = 75.00F;
+            } else if (age >= 18 && age <= 64) {
+                price = 150.00F;
+            } else {
+                price = 112.50F;
+            }
+        }else if (status.equalsIgnoreCase("Premium")) {
+            if (age < 18) {
+                price = 100.00F;
+            } else if (age >= 18 && age <= 64) {
+                price = 200.00F;
+            } else {
+                price = 150.00F;
+            }
+        }
+
+        return price;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int userAge = 0;
+
+        System.out.print("Enter membership status (Regular, VIP, Premium): ");
+        String membershipStatus = scanner.next();
+
+        // will not ask for age anymore
+        if (!(membershipStatus.equalsIgnoreCase("Regular")
+                || membershipStatus.equalsIgnoreCase("VIP")
+                || membershipStatus.equalsIgnoreCase("Premium"))) {
+            System.out.println("Invalid membership status entered.");
+        } else{
+            System.out.print("Enter age: ");
+            userAge = scanner.nextInt();
+
+            float calculatedPrice = calculatePrice(membershipStatus, userAge);
+            System.out.printf("Price: $%.2f%n", calculatedPrice);
+        }
     }
 }
